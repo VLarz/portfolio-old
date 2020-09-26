97fee0ce0e294c4bf1a19583c523967e4c4a3842
Vhen Larson A. Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2020-09-26 21:07:21 +0800

    Added profile section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
