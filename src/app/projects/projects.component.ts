124d3f2bff121c7228d903978ac05051ba7958a7
Vhen Larson A. Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2020-10-08 15:10:33 +0800

    Added carousel on images to project section

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as M from 'materialize-css/dist/js/materialize';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
@@ -9,8 +10,13 @@ export class ProjectsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const carousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(carousel, {
+      fullWidth: true,
+      indicators: true,
+    });
     
   }
 
-
 }
+
