3eb48d271a65021037a18f4d3e9bce63877108e7
Vhen Larson Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2023-01-17 23:18:59 +0800

    Added tailwind element

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    "./src/**/*.{html,ts}" , './node_modules/tw-elements/dist/js/**/*.js'
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('autoprefixer'),
+    require('tw-elements/dist/plugin')
   ],
 }
