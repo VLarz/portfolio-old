c95060cb1f9f5661fb3cf10df05a30ca72c4ab2c
Vhen Larson Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2023-01-18 17:29:02 +0800

    Organize colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}" , './node_modules/tw-elements/dist/js/**/*.js'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('autoprefixer'),
-    require('tw-elements/dist/plugin')
-  ],
-}
+    content: ['./src/**/*.{html,ts}', './node_modules/tw-elements/dist/js/**/*.js'],
+    theme: {
+        fontFamily: {
+            sans: ['Inter', 'sans-serif'],
+            serif: ['Poppins', 'serif'],
+        },
+        extend: {
+            colors: {
+                primary: '#2771D0',
+                secondary: '#022144',
+                gray: '#808899',
+            },
+        },
+    },
+    plugins: [require('autoprefixer'), require('tw-elements/dist/plugin')],
+};
